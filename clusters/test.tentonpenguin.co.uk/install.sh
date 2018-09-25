9ad81b07a2bb23fa071c1163aed5d28f18e350b4
Matt Thorpe <matt.thorpe@itv.com>
2018-09-25 11:33:49 +0100

    moar updates

--- clusters/test.tentonpenguin.co.uk/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo --- Installing test.tentonpenguin.co.uk cluster config...
-
-pushd ~/.kube 2>/dev/null >/dev/null
-kubectl config set-cluster test --server=http://test.tentonpenguin.co.uk
-kubectl config set-context test --cluster=test --user=test-admin
-popd 2>/dev/null >/dev/null
